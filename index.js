8dabc60ca6b45d8cf507a7542c21d05b8ca27bc7
Kesha <kesha.marges@gmail.com>
2022-12-12 18:43:30 -0500

    commit tree 17f1c23dc25463e5dd822bbddc5ab5535c2da888

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, occasion) {
+    let messages = [];
+    
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+};
+
+function countDown() {
+    for (let num = 10; num > -1; num--){
+        console.log(num);
+    }
+}
